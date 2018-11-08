25388fc76f737652fea981cd4432b14e1a470fd5
Joydip Roy <joydipand@gmail.com>
2018-11-08 18:25:16 +0530

    fixing test

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,19 +1,14 @@
 import { shallow } from "enzyme";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 
 import App from "./App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
 
 it("renders welcome message", () => {
   const wrapper = shallow(<App />);
-  const welcome = <h2>Welcome to React</h2>;
-  // expect(wrapper.contains(welcome)).to.equal(true);
-  // expect(wrapper).toContainReact(welcome)
-  expect(wrapper.contains(welcome)).toEqual(true);
+  const welcome = <h1>Welcome to React Typescript Starter</h1>;
+  expect(wrapper.find("h1").contains(welcome)).toEqual(true);
 });
